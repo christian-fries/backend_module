928e4523bcecabfdcba21a8b17c0ec5328e2a35a
Tobias Kummer <tobias.kummer@lstschenker.ch>
2017-03-22 16:38:49 +0100

    [BUGFIX] Adjust TYPO3 dependency in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-0.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
