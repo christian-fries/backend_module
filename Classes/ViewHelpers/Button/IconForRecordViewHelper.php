a2c100184f455687f373306f288d38aa69f23f2c
Christian Fries <christian.fries@lstschenker.ch>
2017-06-28 11:46:31 +0200

    [TASK] Use ContextMenu instead of removed ClickMenu in TYPO3 8

--- Classes/ViewHelpers/Button/IconForRecordViewHelper.php
+++ Classes/ViewHelpers/Button/IconForRecordViewHelper.php
@@ -16,13 +16,14 @@ use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\VersionNumberUtility;
 
 /**
  * ViewHelper to show sprite icon for a record and ClickMenu
  *
  * # Example: Basic example
  * <code>
- * <bm:button.iconForRecord table="tx_myext_domain_model_mymodel" uid="{mymodel.uid}" enableClickMenu="true" />
+ * <bm:button.iconForRecord table="tx_myext_domain_model_mymodel" uid="{mymodel.uid}" enableContextMenu="true" />
  * </code>
  * <output>
  * Icon of the record with the given uid and ClickMenu if enabled.
@@ -41,25 +42,36 @@ class IconForRecordViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\Be\AbstractBa
      *
      * @param string $table table name
      * @param int $uid uid of record
-     * @param boolean $enableClickMenu
+     * @param boolean $enableContextMenu Whether or not the context menu should be opened when clicking on the icon
+     * @param boolean $enableClickMenu Deprecated, use enableContextMenu instead. Will be removed in a future release.
      * @return string sprite icon
      */
-    public function render($table, $uid, $enableClickMenu = true)
+    public function render($table, $uid, $enableContextMenu = true, $enableClickMenu = true)
     {
-        $icon = '';
+        // Compatibility for TYPO3 7: Check enableClickMenu param
+        if (!$enableContextMenu && !$enableClickMenu) {
+            $enableContextMenu = false;
+        }
+
+        $iconTag = '';
         $row = BackendUtility::getRecord($table, $uid);
         if (is_array($row)) {
             /** @var IconFactory $iconFactory */
             $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
-            $icon = '<a title="id=' . $uid . '"';
-            if ($enableClickMenu) {
-                $icon .= ' onclick="TYPO3.ClickMenu.show(\'' . $table . '\', \'' . $uid . '\', \'1\', \'\', \'\', \'\'); return false;"';
+            $iconTag = '<a title="id=' . $uid . '"';
+            if ($enableContextMenu) {
+                if (VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) >= 8007000) {
+                    $iconTag .= ' class="t3js-contextmenutrigger" data-table="' . $table . '" data-uid="' . $uid . '"';
+                }
+                else {
+                    $iconTag .= ' onclick="TYPO3.ClickMenu.show(\'' . $table . '\', \'' . $uid . '\', \'1\', \'\', \'\', \'\'); return false;"';
+                }
             }
-            $icon .= '><span>'
+            $iconTag .= '><span>'
                 . $iconFactory->getIconForRecord($table, $row, Icon::SIZE_SMALL)->render()
                 . '</span></a>';
         }
 
-        return $icon;
+        return $iconTag;
     }
 }
